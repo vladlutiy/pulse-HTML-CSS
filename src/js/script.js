4f0ffb9a15f737708edd7ef2023532ee2ad976ae
Vlad <chillout.ua.mail@gmail.com>
2020-01-11 14:51:04 +0100

    pl jan

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+
+
+
+let number = 6;
+const pi = 3.14;
+let obg = {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+};
+// let answer = prompt("yes?", "");
+// console.log(answer);
+// console.log(4 + "fsf");
+// let isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked && isClose); 
+// console.log(isChecked || isClose); 
+
+// if(3*4 == 8){
+//     console.log("norm   ");
+// }   else{
+//     console.log("ne")
+// }
+
+// let answer = confirm("veselo?");
+// if (answer == true){
+//     console.log("normas");
+// }   else{
+//     console.log("xerovo");
+// }
+
+// const num = 50;
+
+// if (num < 49){
+//     console.log("neverno");
+// } else if(num >= 100){
+//     console.log("mnogo");
+// } else{
+//     console.log("verno");
+// }
+
+let m = 10;
+
+
+for(let i = 1; i < 8; i++){
+    m++;
+    console.log(m);
+}
+
+function loging(a, b){
+    console.log(a + b);
+}
+
+loging(5,3);
